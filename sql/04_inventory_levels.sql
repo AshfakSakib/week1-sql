318b6bf90792c37b13fb5c4631b8f6b3f551459c
AshfakSakib <whateveritisuseless@gmail.com>
2025-11-22 01:03:08 +0100

    Which products are critically low on stock?

--- sql/04_inventory_levels.sql
+++ sql/04_inventory_levels.sql
@@ -1,1 +1,11 @@
+/*
+QUESTION: Which products are critically low on stock?
+*/
 
+SELECT 
+    product_name,
+    units_in_stock,
+    reorder_level
+FROM products
+WHERE units_in_stock < 50 -- Threshold for "Low Stock"
+ORDER BY units_in_stock ASC;
